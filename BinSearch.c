e8e84766fb94b8c67187c6c2d262c6edf8176790
SC2G2YHQ <sc2g2.yhq@gmail.com>
2018-08-28 15:53:00 +0800

    two methods of binary search.

---
+++ BinSearch.c
@@ -0,0 +1,42 @@
+int BinSerach_1(int target,int num[],int length);
+int BinSerach_2(int target,int num[],int length);
+
+int BinSerach_1(int target,int num[],int length)
+{
+	int i=0,
+		left=0,
+		right=length,	//此表示right指向查询域外。则往后要求right=mid;判断条件为left<right，不能包括等于。
+		mid=0;
+						
+	while(left<right){
+		mid=(left+right)/2;
+		if(num[mid]==target){
+			return mid;
+		}
+		if(num[mid]>target){
+			left=mid+1;
+		}else{
+			right=mid;
+		}
+	}
+	return -1;
+}
+
+int BinSerach_2(int target,int num[],int length){
+	int i=0,
+		left=0,
+		right=length-1, //需要考虑到数组长度为1的情况
+		mid=0;
+
+	while(left<=right){
+		mid=(left+right)/2;
+		if(num[mid]==target)
+			return mid;
+		if(num[mid]>target){
+			left=mid+1;
+		}else{
+			right=mid-1;
+		}
+	}
+	return -1;
+}
